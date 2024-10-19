384fb799d88bbe0ea1da7eea248a6006859aeae6
GreenTuGfa <facundofer2607@gmail.com>
2024-10-19 12:17:46 -0300

    Add files via upload

---
+++ Apellido1-Apellido2-Apellido3-Apellido4-TP-P1/src/images/Moneda.java
@@ -0,0 +1,29 @@
+package juego;
+
+import java.awt.Color;
+//import java.awt.Image;
+import entorno.Entorno;
+
+public class Moneda {
+    
+    private int x,y;
+    private int ancho,alto;
+    private int velocidad;
+
+    public Moneda(int x, int y, int ancho, int alto, int velocidad){
+        this.x = x;
+        this.y = y;
+        this.ancho = ancho;
+        this.alto = alto;
+        this.velocidad = velocidad;
+    }
+    public void mover(){
+        this.x = this.x + this.velocidad;
+    }
+    public void cambiarDireccion(){
+        this.velocidad = this.velocidad *(-1);
+    }
+    public void dibujarMoneda(Entorno entorno){
+        entorno.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, 0, Color.YELLOW);
+    }
+}
